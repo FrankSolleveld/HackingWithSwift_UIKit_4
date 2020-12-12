cc21ff7c8fa3c33dc6d93173f9213602671541d5
FrankSolleveld <fsolleveld@outlook.com>
2020-12-12 13:05:05 +0100

    added a bar button + delegate method to update web view title

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -22,11 +22,32 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         let url = URL(string: "https://devfrank.org")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
+    // MARK: -- Custom Methods
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "amazon.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let title = action.title else { return }
+        guard let url = URL(string: "https://" + title) else { return }
+        webView.load(URLRequest(url: url))
+    }
 
+    // MARK: -- Delegate Methods
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
 }
 
